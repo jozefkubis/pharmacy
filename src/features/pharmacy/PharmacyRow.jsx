342dca3b375a3079e07b0831787362e93031cd11
JozefK <164421631+jozefkubis@users.noreply.github.com>
2024-10-21 13:14:19 +0200

    vytvorenie PharmacyRow a PharmacyTable

---
+++ src/features/pharmacy/PharmacyRow.jsx
@@ -0,0 +1,5 @@
+function PharmacyRow() {
+  return <h1>PharmacyRow</h1>
+}
+
+export default PharmacyRow
---
+++ src/features/pharmacy/PharmacyTable.jsx
@@ -0,0 +1,5 @@
+function PharmacyTable() {
+  return <h1>PharmacyTable</h1>
+}
+
+export default PharmacyTable
